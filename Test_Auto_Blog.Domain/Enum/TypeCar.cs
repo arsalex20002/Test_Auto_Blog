22adbad768508ca7128ba2f3249588c22148aea8
arsalex20002 <106921124+arsalex20002@users.noreply.github.com>
2023-06-23 16:44:29 +0300

    Ошибка

--- Test_Auto_Blog.Domain/Enum/TypeCar.cs
+++ Test_Auto_Blog.Domain/Enum/TypeCar.cs
@@ -12,7 +12,7 @@ namespace Test_Auto_Blog.Domain.Enum
         Bus = 2,
         [Display(Name = "Седан")]
         Sedan = 3,
-        [Display(Name = "Спортивная машина")]
+        [Display(Name = "Спортвная машина")]
         SportCar = 4
 
     }
